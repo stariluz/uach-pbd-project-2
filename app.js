c469491acc05f80909ea4a01fb50fc7bfa5609a1
Adrian Gonzalez <adrigondo.profesional@gmail.com>
2024-05-26 14:09:33 -0600

    If else logic fix

--- app.js
+++ app.js
@@ -2,43 +2,47 @@ const http = require('http');
 const fs = require(`fs`);
 
 http.createServer((request, response) => {
-    const file = request.url == '/' ? './WWW/indext.html' : `./WWW/${request.url}` ;       
-if(request.url == '/registro'){
-    let data = [];
-    request.on("data", value => {
-        data.push(value);
-    }).on("end", () => {
-        let params = Buffer.concat(data).toString();
-        response.write(params);
-        response.end();
-    });
-}
-    fs.readFile(file, (err, data)  => {     
-        if(err){
-            response.writeHead(404, {"Content-Type":"text/plain"});
-            response.write("Not found");
+    if (request.url == '/registro') {
+        let data = [];
+        request.on("data", value => {
+            data.push(value);
+        }).on("end", () => {
+            let params = Buffer.concat(data).toString();
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.write(params);
             response.end();
-        } else{
-            const extencion = request.url.split('.').pop();
-            switch(extencion) {
-                case 'txt':
-                    response.writeHead(200, {"Content-Type":"text/plain"});
-                break;
-                case 'html':
-                    response.writeHead(200, {"Content-Type":"text/html"});
-                break;
-                case 'png':
-                    response.writeHead(200, {"Content-Type":"image/png"});
-                break;
-                case 'jpg':
-                    response.writeHead(200, {"Content-Type":"image/jpeg"});
-                break;
-                case 'jpeg':
-                    response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-                break;
+        });
+    } else {
+        const file = request.url == '/'
+            ? './WWW/indext.html'
+            : `./WWW/${request.url}`;
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                response.writeHead(404, { "Content-Type": "text/plain" });
+                response.write("Not found");
+                response.end();
+            } else {
+                const extension = request.url.split('.').pop();
+                switch (extension) {
+                    case 'txt':
+                        response.writeHead(200, { "Content-Type": "text/plain" });
+                        break;
+                    case 'html':
+                        response.writeHead(200, { "Content-Type": "text/html" });
+                        break;
+                    case 'png':
+                        response.writeHead(200, { "Content-Type": "image/png" });
+                        break;
+                    case 'jpg':
+                        response.writeHead(200, { "Content-Type": "image/jpeg" });
+                        break;
+                    case 'jpeg':
+                        response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                        break;
+                }
+                response.write(data);
+                response.end();
             }
-            response.write(data);
-            response.end();
-        }
-    })
+        })
+    }
 }).listen(8888);
